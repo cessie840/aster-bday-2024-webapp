7c4bf91fd8478fdaa5742da3665cb1d97491cd69
Joel Tengco <joeltengco3510@gmail.com>
2024-09-13 07:41:15 -0700

    project popup alignment fixes

--- src/app/views/pages/projects/projects.component.ts
+++ src/app/views/pages/projects/projects.component.ts
@@ -69,13 +69,13 @@ export class ProjectsComponent {
         this.dialog.open(ComicProjectShowcasePopupComponent, {
           data: {
             title: 'Jess Project title',
-            description: '(Description and Project tweet link)',
+            // description: '(Description and Project tweet link)',
+            description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
             imgs: JESS_COMIC_PROJECT_IMAGES,
-            isSmallScreen: this.screenHeight <= 700 / .85,
+            isSmallScreen: this.screenHeight <= 700 / .85 || this.screenWidth <= 1200 / .75,
             backgroundColor: 'rgb(104, 92, 204)',
           },
           minWidth: this.screenWidth <= 1200 / .75 ? '500px' : '700px',
-          height: '80%', // try to take as much space to make comic pages big enough to read on screen
           autoFocus: false,
         });
         break;
